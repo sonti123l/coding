9b62e4ef3b70153c14fed5101cbc991681eb9d03
SaiTrishal22 <116090293+sonti123l@users.noreply.github.com>
2026-09-03 23:16:27 +0530

    Added the new program for today

---
+++ LeetCode/03-09-26/firstMissingPositive.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int firstMissingPositive(int *nums, int numsSize);
+
+int main(){
+    
+    int nums_list[5] = {1, 2, 3, 4, 6};
+
+    int length = sizeof(nums_list)/sizeof(nums_list[0]);
+    firstMissingPositive(nums_list, length);
+    return 0;
+}
+
+int firstMissingPositive(int* nums, int numsSize) {
+    for(int i = 0; i < numsSize; i++){
+        while(nums[i]>=1 && nums[i]<=numsSize && nums[i] != nums[nums[i]-1]){
+            int temp = nums[i];
+            nums[i] = nums[nums[i]-1];
+            nums[temp-1] = temp;
+        }
+    }
+    for(int i = 0; i<numsSize; i++){
+        if(nums[i] != i+1){
+            return i+1;
+        }
+    }
+
+    return numsSize+1;
+}
